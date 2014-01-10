f143075f550018a2f6e0b5532079343d8f2e0e3c
Nick Wade <thewadegeek@gmail.com>
2014-01-09 20:57:38 -0600

    Basic UDP Server
    Init Commit. Don't actually use this, it doesn't close sockets cleanly.

--- ChatServer.py
+++ ChatServer.py
@@ -1,87 +1,25 @@
-#!/usr/bin/python           # This is ChatServer.py file
+#!/usr/bin/python
+ 
+###################################################
+### Author: Nick Wade & Andrew Goettler
+### Purpose: A python chatroom server			 		
+### Please provide credit where appropriate. 	
+###################################################
 
-import socket               # Import socket module
+import socket
 import threading
 from threading import Thread
 
-def serverSend(c, userName):
-	global ActiveSession
+def main(openPort):
+	handshake = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	handshake.bind((socket.getsockname(),openPort))
+	while True:
+		data, addr = s.recvfrom(1024)
+		print data
 
-	while ActiveSession:
-		dataToSend = raw_input(userName + " ==> ")
-
-		if dataToSend == "/exit":
-			ActiveSession = False
-			c.send("CC")
-			break
-
-		c.send(dataToSend)
-
-def serverReceive(c, clientName):
-	global ActiveSession
-
-	while ActiveSession:
-		dataReceived =  c.recv(1024)
-
-		if dataReceived == "CC":
-			ActiveSession = False
-			break
-
-		print clientName + " ==> " + dataReceived
-
-def server(userName):
-	global ActiveSession
-	ActiveSession = True
-
-	server = socket.socket()         # Create a socket object
-
-	host = "192.168.0.104" #raw_input("Enter IP address: ") # socket.gethostname() # Get local machine name
-	port = "12322" # raw_input("Enter port number: ")                # Reserve a port for your service.
-	serverAddress = host + ":" + port
-
-	port = int(port) # needed because raw input will result in a string for the port number; an integer is required
-
-	server.bind((host,port)) # binds the socket to that address
-
-	print("Waiting for client...")
-	server.listen(5)
-
-	connection, addr = server.accept() #how do I determine if connection was successful?
-	if connection != -1:
-
-		connection.send("CE")
-
-		# get the client's name
-		clientName = connection.recv(1024)
-
-		# send the server's user name to the client
-		connection.send(userName)
-
-		print("User " + clientName + " connected.")
-
-		sendingThread = threading.Thread(target = serverSend, args = (connection, userName))
-		receivingThread = threading.Thread(target = serverReceive, args = (connection, clientName))
-
-		sendingThread.start()
-		receivingThread.start()
-
-		sendingThread.join()
-		receivingThread.join()
-
-		# Close the socket when done
-		connection.send("CC")
-		connection.close()
-		print("Connection closed.")
-		
-		server.close()
-		print("Server shutting down.")
-
-	else:
-		print("Could not connect.")
-		server.close()
-
-# Main code below ***********************************************************************************	
-		
-userName = raw_input("Enter userName: ")
-server(userName)
-#server("Andrew")
+###################################################
+### Main Entry Point
+###################################################
+if __name__ == "__main__":
+	initPort = 5555
+    main(initPort)
