11f218410bcb849fbcd692b79d29444b8cae71b6
Andrew Goettler <agoettler@gmail.com>
2014-01-09 21:36:40 -0600

    Added very simple client code

--- ChatClient.py
+++ ChatClient.py
@@ -3,3 +3,25 @@
 import socket               # Import socket module
 import threading
 from threading import Thread
+
+def ClientMain(openPort, targetAddress, targetPort):
+
+	# Create a socket and bind it to a port
+	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	client.bind((socket.getsockname(), openPort))
+
+	dataToSend = "Hi, I'm a client!"
+
+	client.sendto(dataToSend, targetAddress, targetPort)
+
+	receivedData = client.recv(1024)
+
+	print ("Data received:" + receivedData)
+
+# Main code below
+
+initOpenPort = 600
+initTargetAddress = 
+initTargetPort = 
+
+ClientMain(initOpenPort, initTargetAddress, initTargetPort)
