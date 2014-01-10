4f906959c8b761dbbde67986de9c7b9ae43ce411
Andrew Goettler <agoettler@gmail.com>
2014-01-10 01:34:50 -0600

    Implemented very simple connecting/disconnecting logic

--- ChatClient.py
+++ ChatClient.py
@@ -11,7 +11,7 @@ import threading
 from threading import Thread
 from collections import namedtuple
 
-# attempting to use named tuples to simplify management of connection signals
+# Attempting to use named tuples to simplify management of connection signals
 #
 # Signal Definitions:
 # "ReqConn" - Request Connection - sent to the server by the client to request a "connection"
@@ -24,20 +24,45 @@ from collections import namedtuple
 Signals = namedtuple('Signals', ['ReqConn', 'AckConn', 'ReqDisconn', 'AckDisconn'] )
 signals = Signals("CC", "UC", "CE", "UD")
 
-def ClientMain(openPort, targetAddress, targetPort):
+def ClientMain(openPort, host, port):
+	global signals
 
 	# Create a socket and bind it to a port
 	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	client.bind((socket.getsockname(), openPort))
 
-	#request a connection from the server
-	
+	# Request a connection from the server
+	client.sendto(signals.ReqConn, host, port)
+
+	# If the server responds to the request, we can proceed
+	receivedData = client.recv(1024)
+	if receivedData == signals.AckConn
+		print ("Connected to server at " + host + ":" + port)
+
+		# Probably do some other stuff in here
+
+		# Request to be disconnected from the server
+		client.sendto(signals.ReqDisconn, host, port)
+		if client.recv(1024) == signals.AckDisconn
+			print("Server acknowledged shutdown request.")
+			client.shutdown(SHUT_WR)
+			client.close()
+			print("Connection closed".)
+		else
+			# We requested to be disconnected from the server, but it didn't respond. This may be a problem.
+			print ("We requested to be disconnected from the server, but it didn't respond. This may be a problem.")
+	else
+		print("Server never responded.")
+		client.shutdown(SHUT_RDWR)
+		client.close
+		print("Connection closed")
+
 # Main code below
 
 initOpenPort = 0
-initTargetAddress = 0
-initTargetPort = 0
+inithost = 0
+initport = 0
 
 #placeholder values used currently
 
-ClientMain(initOpenPort, initTargetAddress, initTargetPort)
+ClientMain(initOpenPort, inithost, initport)
