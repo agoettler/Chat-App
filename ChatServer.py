1b9f3d94564f4f589f3e120169be88506cedfb9e
Nick Wade <thewadegeek@gmail.com>
2014-01-09 21:33:58 -0600

    Pencil in some basic logic.

--- ChatServer.py
+++ ChatServer.py
@@ -10,18 +10,47 @@ import socket
 import threading
 from threading import Thread
 
-def main(openPort):
-	handshake = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	handshake.bind((socket.getsockname(),openPort))
+class ServerClass:
+	clients = []
+	serverPort = 0
+	def __init__(self,initPort):   
+		ServerClass.serverPort = initPort
+
+	def serverPort():
+		return ServerClass.serverPort
+
+	def addClient(self,clientSock):
+		clients.append(clientSock)
+
+	def rmClient(self,clientSock):
+		clients.remove(clientSock)
+
+	def numClient(self):
+   		return len(ServerClass.clients)
+
+   	def isConnected(clientSock):
+   		if clientSock in ServerClass.clients:
+   			return True
+   		else:
+   			return False
+
+def main(openPort,serverInstance):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind((socket.getsockname(),serverInstance.serverPort()))
 	while True:
 		data, addr = s.recvfrom(1024)
-	    # insert fancy logic here
-	    reply = "fancy message"
-	    s.sendto(reply , addr)
+		if serverInstance.isConnected(addr):
+			#Perform this later
+			if data == "CE":
+				serverInstance.rmClient(addr)
+				s.sendto("UD", addr)
+		elif data == "CC":
+			serverInstance.addClient(addr)
+			s.sendto("UC", addr)
 
 ###################################################
 ### Main Entry Point
 ###################################################
 if __name__ == "__main__":
-	initPort = 5555
-    main(initPort)
+	serverInstance = ServerClass(5555)
+    main(serverInstance)
